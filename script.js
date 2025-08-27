2c753d697bd175750c5c796315ac5cc035850721
Cynthia <vaj.cynthia@gmail.com>
2025-08-27 18:25:59 -0500

    Sixth Commit- JS Create Variables

---
+++ script.js
@@ -0,0 +1,5 @@
+// Create Variables
+const breedSelect = document.getElementById('breedSelect');
+const catImage = document.getElementById('catImage');
+const getCatBtn = document.getElementById('generateBtn');
+const breedInfo = document.getElementById('breedInfo');
