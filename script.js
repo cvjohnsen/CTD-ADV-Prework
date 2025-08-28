e9bd0df42047c5e2f4055d58fb5539efd1c1917d
Cynthia <vaj.cynthia@gmail.com>
2025-08-28 00:25:12 -0500

    Twelvth Commit - fetch breed and random cat

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-// Create Variables
-const breedSelect = document.getElementById('breedSelect');
-const catImage = document.getElementById('catImage');
-const getCatBtn = document.getElementById('generateBtn');
-const breedInfo = document.getElementById('breedInfo');
+// Create Variables/ API
+// const breedSelect = document.getElementById('breedSelect');
+// const catImage = document.getElementById('catImage');
+// const getCatBtn = document.getElementById('generateBtn');
+// const breedInfo = document.getElementById('breedInfo');
+const CAT_API_BASE = 'https://api.thecatapi.com/v1';
 
 // Endpoint 1: Load breed endpoint and populate dropdown
-async function loadBreeds() {
-    const response = await fetch('https://api.thecatapi.com/v1/breeds');
-    const breeds = await response.json();
+   fetchBreeds ()
+     .then(breeds => {
+       breeds.forEach(breed => {
+         const option = document.createElement('option');
+         option.value = breed.id;
+         option.textContent = breed.name;
+         breedSelect.appendChild(option);
+       });
+     })
+     .catch(err => console.error('Error can not load breeds:', err));
 
-    breeds.forEach(breed => {
-        const option = document.createElement('option');
-        option.value = breed.id;
-        option.textContent = breed.name;
-        breedSelect.appendChild(option);
-    });
-}
-
-// Endpoint 2: Fetch and display cat image and breed info
-    async function getCat () {
-    const breedId = breedSelect.value;
-    const url = breedId
-    ? `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    : "https://api.thecatapi.com/v1/images/search";
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-  catImage.src = data[0].url;
-  breedInfo.textContent = breedId
-    ? `Breed: ${breedSelect.selectedOptions[0].text}`
-    : "Random Cat!";
+// Fetch and display random cat image
+    button.addEventListener("click", () => {
+    let url = `${CAT_API_BASE}/images/search`;
+    if (select.value) {
+      url += `?breed_ids=${select.value}`;
     }
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        img.src = data[0].url;
+      })
+      .catch(() => alert("Error loading image"));
+  });
+
+// Breeds.html (Breed page)
+
+
+
+// Endpoint 2: Fetch and display random cat image and breed info
 
 // Event Listener
 getCatBtn.addEventListener('click', getCat);
