3fe7c1bcb234377c06bb3d5f7225474f80845a18
Cynthia <vaj.cynthia@gmail.com>
2025-08-27 19:57:46 -0500

    Eighth Commit - Fetch Cat Pics (Endpoint 2)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const catImage = document.getElementById('catImage');
 const getCatBtn = document.getElementById('generateBtn');
 const breedInfo = document.getElementById('breedInfo');
 
-// Load breed endpoint and populate dropdown
+// Endpoint 1: Load breed endpoint and populate dropdown
 async function loadBreeds() {
     const response = await fetch('https://api.thecatapi.com/v1/breeds');
     const breeds = await response.json();
@@ -16,4 +16,20 @@ async function loadBreeds() {
         breedSelect.appendChild(option);
     });
 }
+
+// Endpoint 2: Fetch and display cat image and breed info
+    async function getCat () {
+    const breedId = breedSelect.value;
+    const url = breedId
+    ? `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    : "https://api.thecatapi.com/v1/images/search";
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  catImage.src = data[0].url;
+  breedInfo.textContent = breedId
+    ? `Breed: ${breedSelect.selectedOptions[0].text}`
+    : "Random Cat!";
+    }
 
