c4a2441293ed168b6757cb89a4e09abccdd48003
Cynthia <vaj.cynthia@gmail.com>
2025-08-27 19:12:29 -0500

    Seventh Commit - Load Breed Endpoint (1)

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const breedSelect = document.getElementById('breedSelect');
 const catImage = document.getElementById('catImage');
 const getCatBtn = document.getElementById('generateBtn');
 const breedInfo = document.getElementById('breedInfo');
+
+// Load breed endpoint and populate dropdown
+async function loadBreeds() {
+    const response = await fetch('https://api.thecatapi.com/v1/breeds');
+    const breeds = await response.json();
+
+    breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+    });
+}
+
